6f522ed6110c7ff29241e689169f68752313d390
Cheat <triet.nguyen@enotion.io>
2026-06-16 22:43:56 +0700

    [Update] Creating a Title Component

--- App.js
+++ App.js
@@ -1,25 +1,12 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { getFontFamily } from './assets/fonts/helper';
-import {
-  StyleSheet,
-  Text,
-} from 'react-native';
-
+import Title from './components/Title/Title';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text style={styles.title}>Hello World!</Text>
+      <Title title={'Let’s Explore'} />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  title: {
-    fontSize: 50,
-    fontFamily: getFontFamily('Inter_18pt', '500'),
-  },
-});
-
 export default App;
