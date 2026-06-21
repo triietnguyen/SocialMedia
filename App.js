72fe537bca2f2063b2e326596c21dc7f02f618a8
Cheat <triet.nguyen@enotion.io>
2026-06-21 10:30:00 +0700

    Installing and Setup fontAwesome Icons

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import Title from './components/Title/Title';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import globalStyle from './assets/styles/globalStyle';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Title title={'Let’s Explore'} />
+      <View style={globalStyle.header}>
+        <Title title={'Let’s Explore'} />
+        <TouchableOpacity style={globalStyle.messageIcon}>
+          <FontAwesomeIcon icon={faEnvelope} size={20} color={'#898DAE'} />
+          <View style={globalStyle.messageNumberContainer}>
+            <Text style={globalStyle.messageNumber}>2</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
+
 export default App;
--- components/Title/style.js
+++ components/Title/style.js
@@ -1,10 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import { getFontFamily } from '../../assets/fonts/helper';
 
 const style = StyleSheet.create({
     title: {
         color: '#022150',
-        fontFamily: getFontFamily('Inter', '600'),
+        fontFamily: Platform.select({
+            ios: 'Inter24pt-Bold',
+            android: getFontFamily('Inter_24pt', '700'),
+        }),
+        fontWeight: '700',
         fontSize: 24,
     },
 });
