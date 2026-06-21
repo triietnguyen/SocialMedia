7b3569267295ee255bfd1871429b40dc6a9f4733
Cheat <triet.nguyen@enotion.io>
2026-06-21 10:53:50 +0700

    [Update] User Profile Image in Flatlist

--- components/UserStory/UserStory.js
+++ components/UserStory/UserStory.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {View, Image, Text} from 'react-native';
+import { View, Image, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import style from './style';
+import UserProfileImage from '../UserProfileImage/UserProfileImage';
 const UserStory = props => {
-  return (
-    <View style={style.storyContainer}>
-      <View style={style.userImageContainer}>
-        <Image source={props.profileImage} style={style.image} />
-      </View>
-      <Text style={style.firstName}>{props.firstName}</Text>
-    </View>
-  );
+    return (
+        <View style={style.storyContainer}>
+            <UserProfileImage
+                profileImage={props.profileImage}
+                imageDimensions={65}
+            />
+            <Text style={style.firstName}>{props.firstName}</Text>
+        </View>
+    );
 };
 
 UserStory.propTypes = {
-  firstName: PropTypes.string.isRequired,
-  profileImage: PropTypes.object.isRequired,
+    firstName: PropTypes.string.isRequired,
+    profileImage: PropTypes.any.isRequired,
 };
 
 export default UserStory;
